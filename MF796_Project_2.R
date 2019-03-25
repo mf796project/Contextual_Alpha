07a48b07955b67183491c67c846984ddee151e63
haomiaoy <48928201+haomiaoy@users.noreply.github.com>
2019-03-25 18:04:26 -0400

    hmy

---
+++ MF796_Project_2.R
@@ -0,0 +1,3 @@
+rets=read.csv("D:/R_code/MF796_code/Github/MF796_Project/Contextual_Alpha/data/S&P500_logret.csv")
+caps=read.csv("D:/R_code/MF796_code/Github/MF796_Project/Contextual_Alpha/data/S&P500_cap.csv")
+
