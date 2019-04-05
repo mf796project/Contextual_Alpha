0ac965c1cbdfde95876ca2d5f3c28d1f9d5b9d4d
Yoel <xyshell@foxmail.com>
2019-04-05 09:49:21 -0400

    .

--- cln_tickerlist.py
+++ cln_tickerlist.py
@@ -8,7 +8,7 @@ wrds_sp500_cons = pd.read_csv(wrds_sp500_cons_path)
 inperiod_loc = -(pd.to_datetime(wrds_sp500_cons['thru']) < start_date)
 wrds_sp500_cons = wrds_sp500_cons[inperiod_loc]
 ticker_list = wrds_sp500_cons['co_tic'].dropna().values
-
+ticker_list = list(set(ticker_list))
 # csv for BB
 with open('ticker_list.csv', 'w') as f:
     for ticker in ticker_list:
