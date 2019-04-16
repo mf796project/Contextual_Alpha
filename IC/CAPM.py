733cec27dfb206964b97c2014f6d2eea896432d5
yadixie <48927694+yadixie@users.noreply.github.com>
2019-04-16 08:03:49 -0400

    universal and contextual IC

---
+++ IC/CAPM.py
@@ -0,0 +1,147 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 12 13:08:14 2019
+
+@author: Yadi Xie
+"""
+import pandas as pd
+import numpy as np
+from dateutil.relativedelta import relativedelta
+
+#calculate beta and unsystemetic risk for each company
+
+spy=pd.read_csv("S&P500_index.csv") #daily SPY index
+spy.index=pd.to_datetime(spy["Date"])
+spy=spy.iloc[:,1:]
+spy=spy.dropna(axis=0,how="all") 
+
+rf=pd.read_csv("S&P500_rf.csv")
+rf.index=pd.to_datetime(rf["Date"])
+rf=rf.iloc[:,1:]
+rf=rf.dropna(axis=0,how="all")
+rf=rf.asfreq("D",method="ffill")/251
+
+price=pd.read_csv("S&P500_clsprc.csv") #close price for all stocks
+price.index=pd.to_datetime(price['Date']) 
+price=price.iloc[:,1:] 
+price=price.dropna(axis=1,how="all") # drop stocks with na all the time
+price=price.dropna(axis=0,how="all")  # drop times with na for all stocks
+   
+ret_d=np.log(price.shift(-1)/price)
+ret_d=ret_d.iloc[:-1,]
+
+mret_d=np.log(spy.shift(-1)/spy)
+mret_d=mret_d.iloc[:-1,]
+
+win=3 #define a rolling window with length of three months
+  
+def CAPM(mret_d,ret_d,rf,win):
+    time=mret_d.index&ret_d.index
+    time=time&rf.index
+    ret_d=ret_d.reindex(time) # remain days when both spy and price data exist
+    mret_d=mret_d.reindex(time) # remain days when both spy and price data exist
+    rf=rf.reindex(time)
+    #start=time[0]+relativedelta(month=win//21+1) # rolling starts from the first day of the forth month
+    
+    beta=pd.DataFrame()
+    unsys=pd.DataFrame()
+    index=[19,41]
+    
+    b=pd.DataFrame(index=ret_d.index,columns=ret_d.columns)
+    b=b.fillna(0)
+    un=pd.DataFrame(index=ret_d.index,columns=ret_d.columns)
+    un=un.fillna(0)
+    
+    for i in range(62,len(time)-1):
+        mar=mret_d.loc[time[i]-relativedelta(months=win):time[i+1]]
+        ind=ret_d.loc[time[i]-relativedelta(months=win):time[i+1]]
+        r=rf.loc[time[i]-relativedelta(months=win):time[i+1]]
+        
+        for j in range(len(ind.iloc[0,])):
+            a=mar.join(r)
+            b.iloc[i,j]=np.cov(a.iloc[:,0]-a.iloc[:,1],ind.iloc[:,j]-a.iloc[:,1])[0,1]/np.var(a.iloc[:,0]-a.iloc[:,1])#lm.coef_[0]
+            un.iloc[i,j]=np.var(ind.iloc[:,j]-a.iloc[:,1])-b.iloc[i,j]**2*np.var(a.iloc[:,0]-a.iloc[:,1])
+            
+        if time[i].month<time[i+1].month:
+            if i==83:
+                for t in range(3):
+                    beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i+1]].mean(axis=0),ignore_index=True)
+                    unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i+1]].mean(axis=0),ignore_index=True)
+                
+                index.append(i)
+            else:
+                beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i+1]].mean(axis=0),ignore_index=True)
+                unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i+1]].mean(axis=0),ignore_index=True)
+                index.append(i)
+    
+   
+    beta.index=time[index]
+    unsys.index=time[index]
+    
+    return beta,unsys     
+
+
+beta,unsys=CAPM(mret_d,ret_d,rf,win)
+beta.to_csv("beta.csv")
+unsys.to_csv("unsystemetic risk.csv")
+
+"""
+
+time=mret_d.index&ret_d.index
+time=time&rf.index
+ret_d=ret_d.reindex(time) # remain days when both spy and price data exist
+mret_d=mret_d.reindex(time) # remain days when both spy and price data exist
+    #start=time[0]+relativedelta(month=win//21+1) # rolling starts from the first day of the forth month
+rf=rf.reindex(time)
+
+ret_d=ret_d.iloc[0:200,0:10]
+mret_d=mret_d.iloc[0:200]
+rf=rf.iloc[0:200]
+time=time[0:200]
+
+beta=pd.DataFrame()
+unsys=pd.DataFrame()
+index=[19,41]
+
+b=pd.DataFrame(index=ret_d.index,columns=ret_d.columns)
+b=b.fillna(0)
+    
+un=pd.DataFrame(index=ret_d.index,columns=ret_d.columns)
+un=un.fillna(0)
+for i in range(62,len(time)-1):
+    mar=mret_d.loc[time[i]-relativedelta(months=win):time[i+1]]
+    ind=ret_d.loc[time[i]-relativedelta(months=win):time[i+1]]
+    r=rf.loc[time[i]-relativedelta(months=win):time[i+1]]
+    
+    
+    
+    for j in range(len(ind.iloc[0,])):
+        a=mar.join(r)
+        #lm=LinearRegression().fit(np.array(a.iloc[:,0]-a.iloc[:,1]),np.array(ind.iloc[:,j]-a.iloc[:,1]))           
+        b.iloc[i,j]=np.cov(a.iloc[:,0]-a.iloc[:,1],ind.iloc[:,j]-a.iloc[:,1])[0,1]/np.var(a.iloc[:,0]-a.iloc[:,1])#lm.coef_[0]
+        un.iloc[i,j]=np.var(ind.iloc[:,j]-a.iloc[:,1])-b.iloc[i,j]**2*np.var(a.iloc[:,0]-a.iloc[:,1])
+        
+        
+    if time[i].month<time[i+1].month:
+        if i==83:
+            
+            beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            
+                             
+            index.append(i)
+        else:
+            beta=beta.append(b.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            unsys=unsys.append(un.loc[time[i]-relativedelta(months=1):time[i]].mean(axis=0),ignore_index=True)
+            index.append(i)
+    
+   # beta.index=time[index[2:]]
+    #beta=pd.DataFrame(beta.iloc[0:2,:],beta.index[0:2],columns=beta.columns).append(beta)
+beta.index=time[index]
+unsys.index=time[index]
+    #if time[i].month<time[i+1].month:
+ """   
---
+++ IC/MF796_Proj_contextual.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  6 10:57:04 2019
+
+@author: Haomiao Yu
+"""
+
+from MF796_Proj_onesizefitall import *
+
+import pandas as pd
+import numpy as np
+
+pb=pd.read_csv("S&P500_PX_TO_BOOK_RATIO.csv")
+pb.index=pd.to_datetime(pb["Date"])
+
+pb=pb.fillna(method="bfill",axis=0)
+pb=pb.asfreq('M', method='ffill')    #resampled price to book
+
+def find_IC_cont(constit,rets,factor,context):
+    """given constituent of SPY, log-return of constituent of SPY, factor time series data,
+    and the context indicator time series, this function gives back the IC time series for different
+    context.
+    """
+    IC_low=[]
+    IC_mid=[]
+    IC_high=[]
+    
+    times=constit.index & rets.index
+    times=times & factor.index
+    times=times & context.index                  #same time
+    
+    factor_col=factor.iloc[0,][factor.iloc[0,]!='#N/A Invalid Security']
+    factor_col=factor_col.index[1:]           #factor appliable
+    
+    cont_col=context.iloc[0,][context.iloc[0,]!='#N/A Invalid Security']
+    cont_col=cont_col.index[1:]                  #context appliable 
+    
+    for i in range(len(times)-1):
+        online=constit.loc[times[i+1],][constit.loc[times[i+1],]==1]   #SPY constituent
+        names=online.index[1:] & factor_col
+        names=names & cont_col                     #same constituent
+        
+        rts=rets.loc[times[i+1],names]
+        rts=rts.fillna(0)
+        
+        cont=context.loc[times[i+1],names]
+        cont=cont.fillna(0)
+        cont=cont.sort_values()      #sort by contextual ,from low to high
+        
+        fac=factor.loc[times[i],names]
+        fac=fac.fillna(0)
+        #fac=np.log(fac)
+        
+        increa_index=cont.index
+        
+        increa_index_low=increa_index[:(len(increa_index)//3)]
+        increa_index_mid=increa_index[(len(increa_index)//3):(2*len(increa_index)//3)]
+        increa_index_high=increa_index[(2*len(increa_index)//3):]
+        
+        IC_low.append(np.corrcoef(rts[increa_index_low],fac[increa_index_low])[0,1])
+        IC_mid.append(np.corrcoef(rts[increa_index_mid],fac[increa_index_mid])[0,1])
+        IC_high.append(np.corrcoef(rts[increa_index_high],fac[increa_index_high])[0,1])
+        
+    IC=[IC_low,IC_mid,IC_high]
+    IC=pd.DataFrame(data=IC)
+    IC=IC.T
+        
+    IC.index=times[1:]
+    IC.columns=['LOW','MID','HIGH']
+        
+    return IC
+        
+        
+        
+#IC=find_IC_cont(constit,rets,pb,pb)
+        
+
+import scipy.stats as st
+
+
+        
+"""    
+growth=pd.read_csv("S&P500_NET_INC_GROWTH.csv")
+growth.index=pd.to_datetime(growth['Date'])
+growth=growth.fillna(method="bfill",axis=0)
+growth=growth.asfreq('M',method='ffill')
+
+leve=pd.read_csv("S&P500_TOT_DEBT_TO_TOT_ASSET.csv")
+leve.index=pd.to_datetime(leve['Date'])
+leve=leve.fillna(method="bfill",axis=0)
+leve=leve.asfreq('M',method='ffill')
+leve.iloc[:,1:]=np.log(leve.iloc[:,1:])
+
+
+
+
+
+IC=find_IC_cont(constit,rets,leve,growth)
+st.ttest_ind(IC.iloc[:,0],IC.iloc[:,1])
+"""
