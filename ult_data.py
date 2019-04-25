bf582dffe3fd9c99f3a5888af123dcaa0bd5578c
You Xie <xyshell@bu.edu>
2019-04-25 14:49:05 -0400

    Update ult_data.py

--- ult_data.py
+++ ult_data.py
@@ -1,8 +1,11 @@
 import os
+import numpy as np
 import pandas as pd
 
 path = os.path.abspath('.')
-data_path = os.path.join(path, 'data')
+data_path = os.path.join(path, 'mat_data')
+res_path = os.path.join(path, 'result')
+cont_path = os.path.join(path, 'context_data')
 
 def get_matdata(factor_name):
     df = pd.read_csv(os.path.join(data_path, '_'.join(['S&P500', factor_name+'.csv'])))
