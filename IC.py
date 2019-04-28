94d235040252bcc4a167b6dc81566ec073887904
You Xie <xyshell@bu.edu>
2019-04-27 21:18:19 -0400

    add data_saoxing

--- IC.py
+++ IC.py
@@ -47,5 +47,15 @@ def find_IC(factor, ret, cont_list, limit=4):
     return ICdf
 
     
+if __name__ == '__main__':
+    # Whole Universe
+    ret = get_matdata('EXRET', 'M')
+    constit = get_matdata('constituents', 'M') 
+    eps = get_matdata('5Y_GEO_GROWTH_DILUTED_EPS' ,'M')
 
+    factor = find_IC(eps, ret, [constit])
 
+    # Contextual
+    beta_low = get_matdata('beta_1_2', 'M') # low beta
+
+    factor = find_IC(eps, ret, [constit, beta_low])
